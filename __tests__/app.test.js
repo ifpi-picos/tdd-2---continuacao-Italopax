f74745ea4c0bd43d540a31e37254c6867b4c4086
Italo Paixão Gomes <italo.gomes@mblabs.com.br>
2023-06-06 20:48:08 -0300

    add get, patch, delete and post routes and Tarefas class methods

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,25 +20,24 @@ describe('Testes da API de Tarefas', () => {
     expect(response.status).toBe(201);
     expect(response.text).toBe('Tarefa adicionada com sucesso!');
   });
+  
   test('GET /tarefas - Deve retornar todas as tarefas', async () => {
     const response = await request(server).get('/tarefas');
     expect(response.status).toBe(200);
     expect(response.body).toContainEqual({"concluida": false, "descricao": "Tarefa 1"});
   });  
+
   test('PATCH /tarefas/:indice - Deve marcar uma tarefa como concluída', async () => {
     const response = await request(server).patch('/tarefas/0');
 
     expect(response.status).toBe(200);
     expect(response.text).toBe('Tarefa marcada como concluída!');
   });
+
   test('DELETE /tarefas/:indice - Deve remover uma tarefa', async () => {
     const response = await request(server).delete('/tarefas/0');
 
     expect(response.status).toBe(200);
     expect(response.text).toBe('Tarefa removida com sucesso!');
   });
-
-
-
-
 });
